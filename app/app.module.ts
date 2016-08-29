d9bb9873f703ed0f6b7fba2827c5550d1c23a9de
miaque <miaque@163.com>
2016-08-29 21:04:15 +0800

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Mique on 2016/8/29.
+ */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Mique on 2016/8/29.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
